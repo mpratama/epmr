5eda5e8df0d03f59cd1310a320318fcf63d44339
Arif Mulpratama <arifmul@gmail.com>
2014-12-01 16:26:25 +0700

    lihat pasien ok

--- controller/select.php
+++ controller/select.php
@@ -1,11 +1,5 @@
 <?php
 
-if (isset($_GET['idpasien']) && is_numeric($_GET['idpasien']))
-	{
-		// get the 'id' variable from the URL
-		$idpasien = $_GET['idpasien'];
-		}
-
 $database = new medoo();
 $detail_pasien = $database->select("pasien", [
 	"idpasien",
@@ -14,7 +8,7 @@ $detail_pasien = $database->select("pasien", [
 	"telepon",
 	"usia"
 ],[
-	"id" => $idpasien
+	"idpasien" => $idpasien
 ]);
 
 $app->render('detail_pasien.html.twig', array(
--- vendor/catfan/medoo/medoo.php
+++ vendor/catfan/medoo/medoo.php
@@ -14,9 +14,9 @@ class medoo
 	// For MySQL, MariaDB, MSSQL, Sybase, PostgreSQL, Oracle
 	protected $server = 'localhost';
 
-	protected $username = 'epmr';
+	protected $username = 'pmr';
 
-	protected $password = 'epmr1234';
+	protected $password = 'pmr1234';
 
 	// For SQLite
 	protected $database_file = '';
@@ -26,7 +26,7 @@ class medoo
 
 	protected $charset = 'utf8';
 
-	protected $database_name = 'epmr';
+	protected $database_name = 'pmr';
 
 	protected $option = array();
 
